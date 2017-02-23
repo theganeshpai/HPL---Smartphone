4b71d8ab484c3d47cda56348544eb247a2458672
Ganesh Pai Mangalore <gpaimangalor@umass.edu>
2017-02-22 21:38:45 -0500

    Smartphone Scenario
    Simple pick and grab

---
+++ Assets/HandControllerOverride.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class HandControllerOverride : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    private Animator animator;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        animator.SetBool("isGrabbing", device.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+    }
+}
+
---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    public Transform sphere;
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Touch' on the Trigger");
+        }
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated TouchDown on the Trigger");
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated TouchUp on the Trigger");
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Press' on the Trigger");
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated PressDown on the Trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated PressUp on the Trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("You activated PressUp on the Touchpad");
+            sphere.transform.position = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerStay (Collider col)
+    {
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down Touch");
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released Touch while colliding with " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+        }
+    }
+
+    void tossObject(Rigidbody rigidBody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidBody.velocity = origin.TransformVector(device.velocity);
+            rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        } else
+        {
+            rigidBody.velocity = device.velocity;
+            rigidBody.angularVelocity = device.angularVelocity;
+        }
+        
+    }
+}
